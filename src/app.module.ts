05bbe43562e3b8e1342f8fabfb24169c790bf0ce
felixtitonina <felixtitonina@outlook.com>
2023-10-11 14:55:13 -0300

    first commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ProjectsModule } from './projects/projects.module';
 
 @Module({
-  imports: [ProjectsModule],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
